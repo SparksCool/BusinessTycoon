32dbdfbece9a44ee6ade34baeac029159e44a540
SparksCool <70763188+SparksCool@users.noreply.github.com>
2024-09-13 21:21:53 -0400

    color works :)

--- src/main.c
+++ src/main.c
@@ -1,14 +1,74 @@
-#include <stdio.h>
-#include "ncurses.h"
-#include "main.h" 
+#include "main.h"
+#include <ncurses.h>
+#include <string.h>
 
-int main()
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW *local_win);
+
+int main(void) {
+    // Variables
+    int ch;
+    WINDOW *main_window;
+
+    // Initialize ncurses
+    initscr();
+    // Dont echo when getch is used
+    noecho();
+    // Enable usage of keys like F1, F2, etc
+    keypad(stdscr, TRUE);
+    // Disable line buffering
+    raw();
+
+    if (!has_colors()) {
+        endwin();
+        printf("Color is not supported in this terminal!");
+        return 1;
+    }
+
+    start_color();
+
+    refresh();
+    // Create main window
+    main_window = create_newwin(LINES - 1, COLS - 1, 0, 0);
+
+    ch = getch();
+
+    if (ch == KEY_LEFT) {
+        attron(A_BOLD);
+        char mesg[] = "GAME READY!";
+        mvwprintw(main_window, LINES/2, (COLS-strlen(mesg))/2,"%s", mesg);
+        attroff(A_BOLD);
+    } else {
+        attron(A_ITALIC);
+        char mesg[] = "Unhandled Input: ";
+        mvwprintw(main_window, LINES/2, (COLS-strlen(mesg) + 1)/2,"%s%c", mesg, ch);
+        attroff(A_ITALIC);
+    }
+
+    wrefresh(main_window);
+    getch();
+    endwin();
+
+    return 0;
+}
+
+WINDOW *create_newwin(int height, int width, int starty, int startx) {
+    WINDOW *local_win;
+
+    local_win = newwin(height, width, starty, startx);
+
+    init_pair(1, COLOR_BLACK, COLOR_CYAN);
+    wbkgd(local_win, COLOR_PAIR(1));
+    box(local_win, 0, 0);
+    wrefresh(local_win);
+
+    return local_win;
+}
+
+void destroy_win(WINDOW *local_win)
 {	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
 
-	return 0;
+	wrefresh(local_win);
+	delwin(local_win);
 }
